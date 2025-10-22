f8383518ed7428013f54a49d57a72a8cf1771e90
sofigarnier1 <sofigarnier1@gmail.com>
2025-10-22 18:44:22 -0300

    data.js

--- assets/js/data.js
+++ assets/js/data.js
@@ -1,18 +1,74 @@
 export const productos = [
-  { id: 101, nombre: "Aros Trenzados", descripcion: "De acero dorado.",
-    categoria: "aros", precio: 6900, stock: 8, img: "../assets/img/aros.jpeg" },
-    { id: 102, nombre: "Collar Luna", descripcion: "De acero dorado.",
-    categoria: "collares", precio: 8500, stock: 12, img: "../assets/img/cadema.jpeg" },
-    { id: 103, nombre: "Anillo Chunky", descripcion: "De acero quirurgico.",
-    categoria: "anillos", precio: 4000, stock: 10, img: "../assets/img/anillos.jpeg" },
-    { id: 104, nombre: "Mini Bag Katia", descripcion: "Cartera de eco cuero.",
-    categoria: "carteras", precio: 25000, stock: 5, img: "../assets/img/cartera.jpeg" },
-  { id: 105, nombre: "Mini Bag Lucy", descripcion: "De gamuza, con detalles dorados.",
-    categoria: "carteras", precio: 28000, stock: 3, img: "../assets/img/cartera-gamuza.jpeg" },
-  { id: 106, nombre: "Aros Colgantes de Flor", descripcion: "De acero quirurgico, con dije de flor con strass.",
-    categoria: "aros", precio: 7900, stock: 7, img: "../assets/img/aros-flor.jpeg" },
-  { id: 107, nombre: "Collar Cielo", descripcion: "De acero quirurgico, con un pedacito de cielo.",
-    categoria: "collares", precio: 12000, stock: 9, img: "../assets/img/collar-cielo.jpeg" },
-  { id: 108, nombre: "Anillo Mariposa", descripcion: "De acero dorado, con strass",
-    categoria: "anillos", precio: 4200, stock: 7, img: "../assets/img/anillo-mariposa.jpeg" }
+  {
+    id: 101,
+    nombre: "Aros Trenzados",
+    descripcion: "De acero dorado.",
+    categoria: "aros",
+    precio: 6900,
+    stock: 8,
+    img: "aros.jpeg"
+  },
+  {
+    id: 102,
+    nombre: "Collar Luna",
+    descripcion: "De acero dorado.",
+    categoria: "collares",
+    precio: 8500,
+    stock: 12,
+    img: "cadema.jpeg"
+  },
+  {
+    id: 103,
+    nombre: "Anillo Chunky",
+    descripcion: "De acero quirurgico.",
+    categoria: "anillos",
+    precio: 4000,
+    stock: 10,
+    img: "anillos.jpeg"
+  },
+  {
+    id: 104,
+    nombre: "Mini Bag Katia",
+    descripcion: "Cartera de eco cuero.",
+    categoria: "carteras",
+    precio: 25000,
+    stock: 5,
+    img: "cartera.jpeg"
+  },
+  {
+    id: 105,
+    nombre: "Mini Bag Lucy",
+    descripcion: "De gamuza, con detalles dorados.",
+    categoria: "carteras",
+    precio: 28000,
+    stock: 3,
+    img: "cartera-gamuza.jpeg"
+  },
+  {
+    id: 106,
+    nombre: "Aros Colgantes de Flor",
+    descripcion: "De acero quirurgico, con dije de flor con strass.",
+    categoria: "aros",
+    precio: 7900,
+    stock: 7,
+    img: "aros-flor.jpeg"
+  },
+  {
+    id: 107,
+    nombre: "Collar Cielo",
+    descripcion: "De acero quirurgico, con un pedacito de cielo.",
+    categoria: "collares",
+    precio: 12000,
+    stock: 9,
+    img: "collar-cielo.jpeg"
+  },
+  {
+    id: 108,
+    nombre: "Anillo Mariposa",
+    descripcion: "De acero dorado, con strass",
+    categoria: "anillos",
+    precio: 4200,
+    stock: 7,
+    img: "anillo-mariposa.jpeg"
+  }
 ];
